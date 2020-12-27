1a76be9b0859685e0d0f41b7a1cbec7f51845e97
Glitch (hello-express) <none>
2020-12-27 13:50:00 +0000

    🕷🎀 Checkpoint ./server.js:26710430/1220 ./package.json:26710430/933

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-// server.js
-// where your node app starts
+const dbd = require("dbd.js")
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+ 
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const bot = new dbd.Bot({
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+token: "NzkyNzQ2ODU2NDM3OTA3NDU2.X-iNCw.S0M_tOEW2VadQcEfZ9AyuBDJXNw", 
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+prefix: "N!"
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+})
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+ 
+
+bot.onMessage()
+
+ 
+
+bot.command({
+
+name: "ping", 
+
+code: `Pong! \`$ping\` ms` 
+
+})ing on port " + listener.address().port);
 });
