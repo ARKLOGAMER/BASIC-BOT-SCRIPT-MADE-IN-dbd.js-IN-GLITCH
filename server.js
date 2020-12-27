177e88e0127eb9b996466fedf0186cc2fafe3192
Glitch (hello-express) <none>
2020-12-27 14:24:52 +0000

    🕰🙍 Checkpoint ./server.js:26710430/545

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bot = new dbd.Bot({
 
 token: "NzkyNzQ2ODU2NDM3OTA3NDU2.X-iNCw.S0M_tOEW2VadQcEfZ9AyuBDJXNw", 
 
-prefix: "!"
+prefix: "$getServerVar(prefix)"
 
 })
 
@@ -22,4 +22,19 @@ name: "ping",
 
 code: `Pong! \`$ping\` ms` 
 
+})
+
+bot.command({
+name: "setprefix",
+aliases: ['changeprefix', 'prefix'],
+code: `$author[Success;https://cdn.discordapp.com/attachments/760236507310850102/780441559468474408/6286_tada_animated.gif]
+$description[**Done, my new prefix is** \`$message\`]
+$color[RANDOM]
+$setServerVar[prefix;$message]
+$onlyIf[$message[1]!=;**You have to put a prefix, example** \`$getServerVar[prefix]setprefix /\`]
+$onlyPerms[admin;$customEmoji[Rufy] **You dont have** \`ADMIN\` **perms**]`
+})
+ 
+bot.variables({
+    prefix: "!"
 })
