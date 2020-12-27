e22ffa88d0d49483a80c591629108e18cb7fe235
Glitch (hello-express) <none>
2020-12-27 14:13:22 +0000

    🎅🌵 Checkpoint ./package.json:26710430/589 ./server.js:26710430/535

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bot = new dbd.Bot({
 
 token: "NzkyNzQ2ODU2NDM3OTA3NDU2.X-iNCw.S0M_tOEW2VadQcEfZ9AyuBDJXNw", 
 
-prefix: "N!"
+prefix: "!"
 
 })
 
@@ -22,5 +22,4 @@ name: "ping",
 
 code: `Pong! \`$ping\` ms` 
 
-})ing on port " + listener.address().port);
-});
+})
