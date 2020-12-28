8abf25678c20816104a5595d4875517ea4ad4885
Glitch (hello-express) <none>
2020-12-28 02:58:34 +0000

    🏊🏼🎣 Checkpoint ./trump.js:26710430/285

---
+++ trump.js
@@ -0,0 +1,11 @@
+bot.command({
+
+  name: "trump",
+
+ code:
+$onlyIf[$message!=;Type something!
+$color[$random[111111;999999]]
+
+$image[https://api.no-api-key.com/api/v2/trump?message=$replaceText[$message; ;+;-1]]
+
+});
