3c1018fa570cb8aaf7a09ce10189a58aeebf3be4
Glitch (nesbroffyt) <none>
2020-12-29 06:34:48 +0000

    🦂🕹 Checkpoint ./server.js:26710430/339

--- server.js
+++ server.js
@@ -93,3 +93,21 @@ $description[$queue[1;10]]
 
 $color[$random[1;9999]]`
 });
+
+bot.command({
+ name: "talle",
+ code:   `
+$title[TALLE BY $username]
+$image[https://api.no-api-key.com/api/v2/kick?&kicked=$userAvatar[$mentioned[1]]&kicker=$userAvatar[$authorID]]`
+});
+
+bot.command({
+
+ name: "meme",
+
+ code:   `
+
+$title[MEME!]
+
+$image[https://ctk-api.herokuapp.com/meme/$random[1;500]]`
+});
