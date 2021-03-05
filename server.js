396e41795d37754cf75ff936b419b9205d4dfa70
Glitch (nesbroffyt) <none>
2021-03-05 09:37:25 +0000

    😚👼 Checkpoint ./server.js:26996302/541

--- server.js
+++ server.js
@@ -115,3 +115,22 @@ $title[MEME!]
 
 $image[https://ctk-api.herokuapp.com/meme/$random[1;500]]`
 });
+
+bot.command({
+ name:"clear",
+aliases:['purge'],
+ code:`
+ $deleteIn[5s]
+ $title[Deleted messages]
+ $description[
+Successfully deleted $message[1] messages]
+$footer[By $username[$authorID]]
+$color[RANDOM]
+$addTimestamp
+
+ $clear[$message[1]]
+$onlyIf[$message[1]<=100;❌You cant delete more than 100 message at a time]
+$onlyIf[$message[1]!=;❌Please select a channel to be your welcome channel]
+$onlyIf[$hasPerms[$authorID;managemessages]==true;❌You don't have enough permissions to set the join channel required perm\` managemessages\`]`
+})
+
