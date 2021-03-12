322e24184dd50d79129d8e1b87a05b51c358ffdc
Glitch (nesbroffyt) <none>
2021-03-12 09:12:25 +0000

    🍤🐡 Checkpoint ./server.js:26996302/1532

--- server.js
+++ server.js
@@ -174,3 +174,45 @@ $serverEmojis]
 $color[73C2FB]`
 })
 
+bot.status({
+	text: 'MCRP SERVER',
+	type: 'Watching',
+	time: 12
+})
+
+bot.command({
+	name: 'ticket-setup',
+	code: `
+ $awaitMessages[$authorID;30s;everything;tsp2;Command has been cancelled]
+ $sendMessage[**Which Category Do you want to make For Ticket System.
+ Provide the Category ID. 
+ This Command will be cancelled after** \`30 seconds\`.;no]
+ $onlyPerms[admin;You Dont Have Prem To Use this Command{delete:10s}]
+ $suppressErrors[]`
+});
+
+bot.awaitedCommand({
+	name: 'tsp2',
+	code: `
+**Successfully Setuped Ticket Channel!📫**
+ $setServerVar[ticketchannel;$message]
+ $onlyIf[$channelExists[$message]==true;Provided Category Doesn't Exist{delete:10s}]
+ $onlyIf[$isNumber[$message]==true;Please provide Category ID{delete:10s}]`
+});
+
+bot.command({
+	name: 'ticket',
+	code: `
+$newTicket[ticket-$username[$authorID];{title:Ticket opened!}{description:Hello, <@$authorID>. Here is your ticket!:tickets: Please give the information about your problem or feedback. 
+}{footer:Use close to *close your ticket};$getServerVar[ticketchannel];no;<@$authorID>, I failed to create your ticket! Try again]
+$sendMessage[Ticket Successfully opened! Hello, <@$authorID>. Go to **$toLowercase[#$username$discriminator]** to describe your issue!;Something went wrong]`
+});
+
+bot.command({
+	name: 'close',
+	code: `
+$closeTicket[This is not a ticket]
+$onlyIf[$checkContains[$channelName;ticket]==true;{description:This command can only be used in ticket channel!} {color:RED} {delete:10s}]
+$suppressErrors`
+});
+
