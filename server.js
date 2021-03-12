efbef606df429e67738d120950e6d2a3789ac65f
Glitch (nesbroffyt) <none>
2021-03-12 08:34:01 +0000

    🍀🗃 Checkpoint ./server.js:26996302/2051 ./package.json:26996302/730

--- server.js
+++ server.js
@@ -140,24 +140,39 @@ bot.joinCommand({
 })
 bot.onJoined()
 
-bot.command({
- name: "modmail",
- code: `$color[RANDOM]
- $useChannel[818342046703157250]
-$description[<@$authorID> $message]
-$title[$username\`($authorID)\` sent modmail - to respond type <reply @user message>]
-$addCmdReactions[:incoming_envelope:]
-$cooldown[1m;:no_entry: Modmail is on cooldown. Please try again in 1 minutes]`
-})
 
 bot.command({
- name: "reply",
- code: `$title[Modmail Reply]
-$dm[$mentioned[1]]
-$color[RANDOM]
-$description[You've received a new reply to your modmail.
-$message]
-$addCmdReactions[:package:]
-$onlyPerms[admin;Only Users with \`ADMIN\` perms can use this]
-$suppressErrors[Make sure to mention an user]`
-})
+	name: 'ticket-setup',
+	code: `
+ $awaitMessages[$authorID;30s;everything;tsp2;Command has been cancelled]
+ $sendMessage[**Which Category Do you want to make For Ticket System.
+ Provide the Category ID. 
+ This Command will be cancelled after** \`30 seconds\`.;no]
+ $onlyPerms[admin;You Dont Have Prem To Use this Command{delete:10s}]
+ $suppressErrors[]`
+});
+
+bot.awaitedCommand({
+	name: 'tsp2',
+	code: `
+**Successfully Setuped Ticket Channel!📫**
+ $setServerVar[ticketchannel;$message]
+ $onlyIf[$channelExists[$message]==true;Provided Category Doesn't Exist{delete:10s}]
+ $onlyIf[$isNumber[$message]==true;Please provide Category ID{delete:10s}]`
+});
+
+bot.command({
+	name: 'ticket',
+	code: `
+$newTicket[ticket-$username[$authorID];{title:Ticket opened!}{description:Hello, <@$authorID>. Here is your ticket!:tickets: Please give the information about your problem or feedback. 
+}{footer:Use close to *close your ticket};$getServerVar[ticketchannel];no;<@$authorID>, I failed to create your ticket! Try again]
+$sendMessage[Ticket Successfully opened! Hello, <@$authorID>. Go to **$toLowercase[#$username$discriminator]** to describe your issue!;Something went wrong]`
+});
+
+bot.command({
+	name: 'close',
+	code: `
+$closeTicket[This is not a ticket]
+$onlyIf[$checkContains[$channelName;ticket]==true;{description:This command can only be used in ticket channel!} {color:RED} {delete:10s}]
+$suppressErrors`
+});
